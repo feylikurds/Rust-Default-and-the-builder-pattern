da9ec73b07beb8c2486023b0ba0a995d75ebcb8d
feylikurds <feylikurds@gmail.com>
2023-12-12 16:20:12 +0100

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Rust-Default-and-the-builder-pattern
 Rust: Default and the builder pattern
+
+## Purpose
+To show others an example of Rust programming.
+
+## License
+Copyright (c) 2023 Aryo Pehlewan aryopehlewan@hotmail.com 
+Licensed under the GPL license.
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+/*
+Rust: Default and the builder pattern
+Copyright (C) 2023 Aryo Pehlewan aryopehlewan@hotmail.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#[derive(Debug)]
+struct Character {
+    name: String,
+    age: u8,
+    height: u32,
+    weight: u32,
+    lifestate: LifeState,
+    can_use: bool, // Here is the new value
+}
+
+#[derive(Debug)]
+enum LifeState {
+    Alive,
+    Dead,
+    NeverAlive,
+    Uncertain,
+}
+
+impl Character {
+    fn new() -> Self {
+        Self {
+            name: "Billy".to_string(),
+            age: 15,
+            height: 170,
+            weight: 70,
+            lifestate: LifeState::Alive,
+            can_use: true,  // .new() makes a fine character, so it is true
+        }
+    }
+
+    fn height(mut self, height: u32) -> Self {
+        self.height = height;
+        self.can_use = false; // Now the user can't use the character
+        self
+    }
+
+    fn weight(mut self, weight: u32) -> Self {
+        self.weight = weight;
+        self.can_use = false;
+        self
+    }
+
+    fn name(mut self, name: &str) -> Self {
+        self.name = name.to_string();
+        self.can_use = false;
+        self
+    }
+
+    fn build(mut self) -> Result<Character, String> {
+        if self.height < 200 && self.weight < 300 && !self.name.to_lowercase().contains("smurf") {
+            self.can_use = true;   // Everything is okay, so set to true
+            Ok(self)               // and return the character
+        } else {
+            Err("Could not create character. Characters must have:
+1) Height below 200
+2) Weight below 300
+3) A name that is not Smurf (that is a bad word)"
+                .to_string())
+        }
+    }
+}
+
+fn main() {
+    let character_with_smurf = Character::new().name("Lol I am Smurf!!").build(); // This one contains "smurf" - not okay
+    let character_too_tall = Character::new().height(400).build(); // Too tall - not okay
+    let character_too_heavy = Character::new().weight(500).build(); // Too heavy - not okay
+    let okay_character = Character::new()
+        .name("Billybrobby")
+        .height(180)
+        .weight(100)
+        .build();   // This character is okay. Name is fine, height and weight are fine
+
+    // Now they are not Character, they are Result<Character, String>. So let's put them in a Vec so we can see them:
+    let character_vec = vec![character_with_smurf, character_too_tall, character_too_heavy, okay_character];
+
+    for character in character_vec { // Now we will print the character if it's Ok, and print the error if it's Err
+        match character {
+            Ok(character_info) => println!("{:?}", character_info),
+            Err(err_info) => println!("{}", err_info),
+        }
+        println!(); // Then add one more line
+    }
+}
